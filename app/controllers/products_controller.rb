57304e90719a7a570865d3110b5b4d8e303a48a5
Victoria Meng <nights1ststar@protonmail.com>
2018-09-12 01:24:03 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,7 +25,7 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
     respond_to do |format|
-      format.html { render :show }
+      format.html { render :'products/show' }
     end
   end
 

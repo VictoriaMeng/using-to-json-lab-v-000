bbbf8c0e34e0d89961b6d1738db949a0f5fc3588
Victoria Meng <nights1ststar@protonmail.com>
2018-09-12 01:24:50 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,7 +25,7 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
     respond_to do |format|
-      format.html { render :'products/show' }
+      format.json { render json: @product.to_json(only: [:id, :name, :description, :inventory, :price] }
     end
   end
 
